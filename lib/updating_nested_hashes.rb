8443ddae1a4446743fc2a44279a9f03acad35c80
eager-class-5814 <candacehchoe@gmail.com>
2020-05-24 02:13:12 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,27 +1,25 @@
-epic_tragedy = {
+epic_tragedy[:montague][:patriarch][:name] = "Michael Jordan"
+ 
+puts epic_tragedy
+ 
+#  =>
+{
    :montague => {
-      :patriarch => {name: "Lord Montague", age: "53"},
+      :patriarch => {name: "Michael Jordan", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-         {name: "Benvolio", age: "17", attitude: "worried"},
-         {name: "Mercutio", age: "18", attitude: "hot-headed"}
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
    },
-   
-      epic_tragedy[:montague][:hero][:status] = "dead"
-      epic_tragedy[:montague][:heroine][:status] = "dead"
-
-    
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Steven", age: "30", attitude: "confused"},
-          {name: "Nurse", age: "44", attitude: "worried"}
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
-
-epic_tragedy[:additional_characters] = [prince = {name: "Prince Escalus"}, apothecary = {name: "Apothecary"}]
