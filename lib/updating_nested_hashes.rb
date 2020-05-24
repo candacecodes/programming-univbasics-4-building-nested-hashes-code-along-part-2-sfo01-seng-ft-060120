8362d393533cccb201f98f20d32e4612663fa19a
eager-class-5814 <candacehchoe@gmail.com>
2020-05-24 02:47:43 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -1,32 +1,5 @@
-def update_status
-  epic_tragedy { 
-   :montague => {
-      :patriarch => {name: "Lord Mon", age: "53"},
-      :matriarch => {name: "Lady Montague", age: "54"},
-      :hero => {name: "Romeo", age: "15", status: "alive"},
-      :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"},
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}
-      ]
-   },
-   :capulet => {
-      :patriarch => {name: "Lord Capulet", age: "50"},
-      :matriarch => {name: "Lady Capulet", age: "51"},
-      :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried"}
-      ]
-   }
-}
-   epic_tragedy[:motague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
-
-}
-
-def additional_characters
+def update_status 
   epic_tragedy = {
-      epic_tragedy { 
    :montague => {
       :patriarch => {name: "Michael Jordan", age: "53"},
       :matriarch => {name: "Lady Montague", age: "54"},
@@ -45,8 +18,34 @@ def additional_characters
         {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-      }
-    
+}
+epic_tragedy[:motague][:hero][:status] = "dead"
+epic_tragedy[:capulet][:heroine][:status] = "dead"
+epic_tragedy 
+end 
+
+def fix_roles 
+   epic_tragedy = {
+   :montague => {
+      :patriarch => {name: "Michael Jordan", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => [
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
+      ]
+   },
+   :capulet => {
+      :patriarch => {name: "Lord Capulet", age: "50"},
+      :matriarch => {name: "Lady Capulet", age: "51"},
+      :heroine => {name: "Juliet", age: "15", status: "alive"},
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+        {name: "Nurse", age: "44", attitude: "worried"}
+      ]
+   }
+}
 epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
 epic_tragedy[:additional_characters] << { name: "Apothecary" }
-end 
+ epic_tragedy
+end
