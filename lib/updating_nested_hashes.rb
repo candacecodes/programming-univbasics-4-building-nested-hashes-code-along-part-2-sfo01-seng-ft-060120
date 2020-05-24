21cd8e037e6f7d469507975af512f8db52df2133
eager-class-5814 <candacehchoe@gmail.com>
2020-05-24 02:56:08 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -21,7 +21,7 @@ def update_status
 }
 epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
-puts epic_tragedy 
+epic_tragedy 
 end 
 
 def fix_roles 
