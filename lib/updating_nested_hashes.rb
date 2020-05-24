59c13d4728614ad44ee53ded88578e91687e6366
eager-class-5814 <candacehchoe@gmail.com>
2020-05-24 02:54:43 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -45,7 +45,4 @@ def add_characters
       ]
    }
 }
-epic_tragedy[:additional_characters] << { name: "Prince Escalus" }
-epic_tragedy[:additional_characters] << { name: "Apothecary" }
-epic_tragedy
 end
